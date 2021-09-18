f1bf4e12ab9bc0ffddc6d9a91dac6ee2c57cf441
Leonid Kovtun <turion125@gmail.com>
2021-09-18 15:19:34 +0300

    Lab 1

---
+++ Restoran/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace Restoran
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить - >";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(335, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 68);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "< - Убрать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(335, 305);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 68);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Заказать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Items.AddRange(new object[] {
+            "lst Заказ"});
+            this.listBox2.Location = new System.Drawing.Point(523, 82);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(247, 356);
+            this.listBox2.TabIndex = 4;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "90р - Глазунья ",
+            "250р - Гусь жаренный ",
+            "100р - Зеленый горошек ",
+            "100р - Кильки в том. соусе ",
+            "20р - Компот ",
+            "150р - Мундиры ",
+            "550р - Омар ",
+            "600р - Суши ",
+            "25р - Сухарики ",
+            "90р - Цветная капуста ",
+            "85р - Не цветная капуста "});
+            this.listBox1.Location = new System.Drawing.Point(12, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(247, 356);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 34);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "МЕНЮ:\r\n";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(523, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(206, 34);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "ЗАКАЗ:\r\n";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(347, 397);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Ресторан \"Студент\"";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Restoran/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restoran
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox3.Text = 0.ToString() + "р";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        
+      
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+               
+                    object item = listBox1.SelectedItem;
+                    if (item != null)
+                    {
+                        char ind = item.ToString()[0];
+                        string partSumm = ind.ToString();
+                        int i = 1;
+                        while (ind != 'р')
+                        {
+                            ind = item.ToString()[i];
+                            partSumm = partSumm + ind.ToString();
+                            i++;
+                        }
+                        partSumm = partSumm.Replace("р", string.Empty);
+                        textBox3.Text = textBox3.Text.Replace("р", string.Empty);
+                        textBox3.Text = (int.Parse(textBox3.Text) + int.Parse(partSumm)).ToString() + "р";
+
+                        listBox2.Items.Add(item);
+                    
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex != -1)
+            {
+                object item = listBox2.SelectedItem;
+                if (item != null)
+                {
+                    char ind = item.ToString()[0];
+                    string partSumm = ind.ToString();
+                    int i = 1;
+                    while (ind != 'р')
+                    {
+                        ind = item.ToString()[i];
+                        partSumm = partSumm + ind.ToString();
+                        i++;
+                    }
+                    partSumm = partSumm.Replace("р", string.Empty);
+                    textBox3.Text = textBox3.Text.Replace("р", string.Empty);
+                    textBox3.Text = (int.Parse(textBox3.Text) - int.Parse(partSumm)).ToString() + "р";
+
+                    listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int number = listBox2.Items.Count;
+            if (number > 1) 
+            {
+                DialogResult result = MessageBox.Show("Все заказали, вы уверены?",
+    "Ресторан «Студент»", MessageBoxButtons.YesNo, MessageBoxIcon.Information,
+    MessageBoxDefaultButton.Button3, MessageBoxOptions.DefaultDesktopOnly);
+                if (result == DialogResult.Yes) { MessageBox.Show("С богом! Cумма заказа =" + textBox3.Text, "Ресторан «Студент»",
+                 MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
+
+                else
+                {
+                    MessageBox.Show("Давайте еще разок!", "Ресторан «Студент»",
+                   MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            
+            }
+            else
+            {
+                MessageBox.Show("Ваш заказ пуст!", "Ресторан «Студент»",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
